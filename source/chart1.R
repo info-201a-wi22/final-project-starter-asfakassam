a8140b1619b35ce102e794080f44859ec7e5bc96
ShengyueGao <shengg6@uw.edu>
2022-02-18 12:41:41 -0800

    changes

--- source/chart1.R
+++ source/chart1.R
@@ -2,17 +2,28 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 
-crime_state <- read.csv(
-  file = "data/state_crime.csv",
-)
+crime_state <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-asfakassam/main/data/state_crime.csv")
 
 plot_data <- crime_state %>%
   filter(Year >= 2005 & Year <= 2011)%>%
-  select(State, Year, Data.Totals.Violent.All)%>%
+  select(State, Year, Data.Totals.Violent.All)
+
+before_rec <- c(2005:2006)
+during_rec <- c(2007:2009)
+after_rec <- c(2010:2011)
+
+plot_data$Year[plot_data$Year %in% before_rec] <- "Before Recession"
+plot_data$Year[plot_data$Year %in% during_rec] <- "During Recession"
+plot_data$Year[plot_data$Year %in% after_rec] <- "After Recession"
+
+plot_data <- plot_data %>%
+  group_by(State, Year) %>%
+  summarise(violent_crimes = sum(Data.Totals.Violent.All))
+View(plot_data)
 
 crime_plot <- ggplot(plot_data) + 
   geom_col(
-    mapping = aes(x = State, y = Data.Totals.Violent.All, fill = Year),
+    mapping = aes(x = State, y = violent_crimes, fill = Year),
   )
   
 bar_chart <- ggplotly(crime_plot)
