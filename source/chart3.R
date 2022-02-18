2f08b4255abd12cc3a29b4acc19714f360c4dec9
“Yinnk” <kying2@uw.edu>
2022-02-18 15:54:50 -0800

    complete changes

---
+++ source/chart3.R
@@ -0,0 +1,44 @@
+#load packages
+library(ggplot2)
+library(maps)
+library(usdata)
+
+#1)load dataset
+data <-read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-asfakassam/main/data/state_crime.csv",stringsAsFactors = FALSE)
+
+#2)select dataset range we want to focus-year 2008
+
+crime_data_2008 <-data %>% 
+  filter(Year=="2008") %>% 
+  select(State,Data.Totals.Property.All,Data.Totals.Violent.All) %>% 
+  rename(state=State)
+  
+
+#3) plot a blank US map
+
+state_shape <- map_data("state") %>%
+  rename(state = region)
+#Step 3: Prepare state_shape for joining by modifying state to be abbreviations (not full names)
+state_shape <- state_shape %>%
+  mutate(state = state2abbr(state))
+
+#Step 4: Join your state_shape to your dataframe (replace data with the name of your dataframe)
+state_shape <- state_shape %>%
+  left_join(crime_data_2008, by = "state")
+
+#Step 5: Create map (replace values with the column name of the values you want to display)
+plot <- ggplot(state_shape) +
+  geom_polygon(
+    mapping = aes(x = long, y = lat, group = group, fill = Data.Totals.Property.All)
+  )
+
+
+
+
+
+
+
+ 
+
+ 
+  
---
+++ source/summary.R
@@ -0,0 +1,79 @@
+#load packages
+library("dplyr")
+
+#1)import dataset
+data <-read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-asfakassam/main/data/state_crime.csv",stringsAsFactors = FALSE)
+
+#2)select dataset range we want to focus-year 2005 to year 2011
+
+crime_data <-data %>%
+  filter(Year=="2005"|Year=="2006"|Year=="2007"|Year=="2008"|Year=="2009"|Year=="2010"|Year=="2011")
+
+#3)summarize information into a list
+summary <-list()
+
+#4)summarize the total number of rows we focus in the crime_data data frame.
+summary$total_num_rows <-nrow(crime_data)
+
+#5)summarize the number of differnt states in the crime_data data frame.
+summary$num_state <-crime_data %>% 
+  select(State) %>% 
+  unique() %>% 
+  nrow()
+
+#6)summarize the number of years we focus
+summary$num_years <-crime_data %>% 
+  select(Year) %>% 
+  unique() %>% 
+  nrow()
+
+#7)summarize the most number of property crime in a state in 7 years
+summary$most_property <-crime_data %>% 
+  select(Data.Totals.Property.All) %>% 
+  max()
+
+#8)summarize the most number of violent crime in a state in 7 years
+summary$most_violent <-crime_data %>% 
+  select(Data.Totals.Violent.All) %>% 
+  max()
+
+#9)find the state with highest number of property crime in each year
+summary$highest_prop_state <-crime_data %>% 
+  group_by(Year) %>% 
+  filter(Data.Totals.Property.All==max(Data.Totals.Property.All)) %>% 
+  select(State, Year)
+
+#10)find the state with lowest number of property crime in each year
+summary$lowest_prop_state <-crime_data %>% 
+  group_by(Year) %>% 
+  filter(Data.Totals.Property.All==min(Data.Totals.Property.All)) %>% 
+  select(State, Year)
+
+#11)find the state with highest number of violent crime in each year
+summary$highest_vio_state <-crime_data %>% 
+  group_by(Year) %>% 
+  filter(Data.Totals.Violent.All==max(Data.Totals.Violent.All)) %>% 
+  select(State, Year)
+
+#12)find the state with lowest number of violent crime in each year
+summary$lowest_vio_state <-crime_data %>% 
+  group_by(Year) %>% 
+  filter(Data.Totals.Violent.All==min(Data.Totals.Violent.All)) %>% 
+  select(State, Year)
+  
+
+#13)find the total number of property crime for all states in each year
+summary$total_prop <-crime_data %>% 
+  group_by(Year) %>%
+  summarise(total_property_crime=sum(Data.Totals.Property.All))
+
+#14)find the total number of violent crime for all states in each year
+summary$total_vio <-crime_data %>% 
+  group_by(Year) %>%
+  summarise(total_violent_crime=sum(Data.Totals.Violent.All))
+
+
+
+
+
+
