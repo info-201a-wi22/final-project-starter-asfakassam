47ccae79b407e00c88c506d01a6376e57d7aad15
Stark Yin <jyin2@uw.edu>
2022-02-18 23:37:55 -0800

    Third Edition of the chart2

--- source/chart2.R
+++ source/chart2.R
@@ -1,10 +1,11 @@
+# 1) load the library and Dataset 
 library(ggplot2)
 library(dplyr)
 library(plotly)
-
 crime_state <- read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-asfakassam/main/data/state_crime.csv")
 
-plot_data <- crime_state %>%
+# 2) Sort and classify the data into before, during, and after the Recession
+sorted_data <- crime_state %>%
   filter(Year >= 2005 & Year <= 2011)%>%
   select(State, Year, Data.Totals.Property.All)
 
@@ -12,23 +13,23 @@ before_rec <- c(2005:2006)
 during_rec <- c(2007:2009)
 after_rec <- c(2010:2011)
 
-plot_data$Year[plot_data$Year %in% before_rec] <- "Before Recession"
-plot_data$Year[plot_data$Year %in% during_rec] <- "During Recession"
-plot_data$Year[plot_data$Year %in% after_rec] <- "After Recession"
+sorted_data$Year[sorted_data$Year %in% before_rec] <- "Before the Recession"
+sorted_data$Year[sorted_data$Year %in% during_rec] <- "During the Recession"
+sorted_data$Year[sorted_data$Year %in% after_rec] <- "After the Recession"
 
-plot_data <- plot_data %>%
+sorted_data <- sorted_data %>%
   group_by(State, Year) %>%
   summarise(property_crimes = sum(Data.Totals.Property.All))
-View(plot_data)
+View(sorted_data)
 
-crime_plot <- ggplot(plot_data) + 
+crime_plot <- ggplot(sorted_data) + 
   geom_point(
     mapping = aes(x = State, y = property_crimes, fill = Year),
   )
 
-bar_chart <- ggplotly(crime_plot)
+scatter_plot <- ggplotly(crime_plot)
 
-crime_plot <- bar_chart %>% layout(
+crime_plot <- scatter_plot %>% layout(
   title = "Scatter Plot of Total Number of Property Crime in Each State (2005-2011)",
   xaxis = list(title = "State"),
   yaxis = list(title = "Total Number of Property Crime")
