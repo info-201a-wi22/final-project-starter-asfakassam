45161d50cd5d970136f8312f8f310e312d9af933
“Yinnk” <kying2@uw.edu>
2022-02-18 17:48:19 -0800

    change ploting data

--- source/chart3.R
+++ source/chart3.R
@@ -4,6 +4,7 @@ library(maps)
 library(usdata)
 library(dplyr)
 library(plotly)
+
 #1)load dataset
 data <-read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-starter-asfakassam/main/data/state_crime.csv",stringsAsFactors = FALSE)
 
@@ -12,27 +13,30 @@ data <-read.csv("https://raw.githubusercontent.com/info-201a-wi22/final-project-
 crime_data_2008 <-data %>% 
   filter(Year=="2008") %>% 
   select(State,Data.Totals.Property.All,Data.Totals.Violent.All) %>% 
-  rename(state=State)
+  rename(state=State) %>% 
+  mutate(total_crime=Data.Totals.Property.All+Data.Totals.Violent.All)
   
 
-#3) plot a blank US map
+#3) Create US shapefile and rename region column to state
 
 state_shape <- map_data("state") %>%
   rename(state = region)
-#Step 3: Prepare state_shape for joining by modifying state to be abbreviations (not full names)
+
+#4) Prepare state_shape for joining by modifying state to be abbreviations.
 state_shape <- state_shape %>%
   mutate(state = state2abbr(state))
 
-#Step 4: Join your state_shape to your dataframe (replace data with the name of your dataframe)
+#4): Join state_shape to crime_data_2008 dataframe
 state_shape <- state_shape %>%
   left_join(crime_data_2008, by = "state")
 
-#Step 5: Create map (replace values with the column name of the values you want to display)
+#5): Create a map
 plot <- ggplot(state_shape) +
   geom_polygon(
-    mapping = aes(x = long, y = lat, group = group, fill = Data.Totals.Property.All)
+    mapping = aes(x = long, y = lat, group = group, fill = total_crime)
   )
 
+#Make the map interactive 
 ggplotly(plot)
 
 
